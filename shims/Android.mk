3f0a5ffb6ad14474e9d03f4f203f40b9a07ecb28
A2L5E0X1 <alexed25@protonmail.com>
2022-08-18 19:39:17 +0200

    exynos850-common: relocate sensorndkbridge shim
    Change-Id: Ia53869fd1e17787c49d4e26bff3593a700a773ef

--- shims/Android.mk
+++ shims/Android.mk
@@ -6,4 +6,20 @@
 
 LOCAL_PATH := $(call my-dir)
 
-include $(call all-makefiles-under,$(LOCAL_PATH))
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := ASensorManager.cpp
+LOCAL_SHARED_LIBRARIES := \
+    libbase \
+    libsensorndkbridge \
+    libutils
+LOCAL_MODULE := libshim_sensorndkbridge
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_VENDOR_MODULE := true
+
+LOCAL_C_INCLUDES += system/core/base/include
+LOCAL_C_INCLUDES += system/core/libutils/include
+LOCAL_C_INCLUDES += frameworks/hardware/interfaces/sensorservice/libsensorndkbridge
+
+include $(BUILD_SHARED_LIBRARY)
